d888c71d42808f26f45eeb279e5ab08beab6f721
atomamr <haanamr7@gmail.com>
2025-10-29 08:44:38 +0530

    chep11ternaryoprator

---
+++ chep11ternaryoprator/index.js
@@ -0,0 +1,15 @@
+// let age = 19;
+// let message = age >= 18 ? "you are an adult" : "You are a minor";
+// console.log(message);
+
+// let time = 12;
+// let greeting = time < 12 ? "Good morning" : "Good afternoon";
+// console.log(greeting)
+
+// let isStudent = false;
+// let message = isStudent ? "You are a Student" : "You are not a Student";
+// console.log(message);
+
+let  purcherseAmount = 125;
+let discount = purcherseAmount >= 100 ? 10 : 0;
+console.log(`Your total is $${purcherseAmount - purcherseAmount * (discount/100)}`)

2696939e4a42aa01bb7a04ec340514826dd19f42
atomamr <haanamr7@gmail.com>
2025-11-07 06:55:25 +0530

    chap17strictporator

---
+++ chap17strictoprator/index.js
@@ -0,0 +1,14 @@
+// == Assigment oprator 
+// Comparison oprator (compare if values are euqal)
+// === Strict equality oprator (compare if values & datatypes are equal)
+// != inequality oprator
+// !== strict inequality oprator
+
+const PI = "3.14";
+
+if(PI !== "3.14"){
+    console.log("That is NOT Pi");
+}
+else{
+    console.log("That is Pi");
+}

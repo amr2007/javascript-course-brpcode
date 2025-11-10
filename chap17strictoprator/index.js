d80ae40a0167088c300a3258b85de63fc914c2eb
atomamr <haanamr7@gmail.com>
2025-11-10 06:56:04 +0530

    chap19ForLoop

--- chap17strictoprator/index.js
+++ chap17strictoprator/index.js
@@ -6,9 +6,9 @@
 
 const PI = "3.14";
 
-if(PI !== "3.14"){
+if (PI !== "3.14") {
     console.log("That is NOT Pi");
 }
-else{
+else {
     console.log("That is Pi");
 }
---
+++ chap19ForLoop/index.js
@@ -0,0 +1,26 @@
+// For Loop = repeat some code a limited amount of times 
+// for(let i = 10; i > 0; i-=2){
+//     console.log(i);
+// }
+
+// console.log("Heppy New Year")
+
+// for(let i = 1; i <= 20; i++){
+//     if(i == 13){
+//         continue; // when we want to skip an ittretation we will use continue 
+//     }
+//     else{
+//         console.log(i)
+//     }
+
+
+// }
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 13) {
+        break; // when we want to get out of loop at certain ittretation
+    }
+    else {
+        console.log(i);
+    }
+}
